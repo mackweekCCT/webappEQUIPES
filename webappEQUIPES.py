c1bc1c95999881312d7afa3e77d0b238d3ef7179
lpgroup <68185745+lpgroup@users.noreply.github.com>
2022-09-18 05:43:27 -0300

    Update webappEQUIPES.py

--- webappEQUIPES.py
+++ webappEQUIPES.py
@@ -226,8 +226,8 @@ elif choice == "EQUIPE 01":
     st.header("Painel Analítico: DÚVIDAS E RESPOSTAS")  
     col1, col2 = st.columns((1,1))
     with col1:
-        st.warning("Nº de Dúvidas:")
-        st.info(NregD)
+        st.write("Nº de Dúvidas:")
+        st.warning(NregD)
     with col2:
         st.write("Nº de dúvidas RESPONDIDAS:")
         st.info(NregR)
