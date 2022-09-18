f14f8cc05637c77eae77d23913246e560cb13104
lpgroup <68185745+lpgroup@users.noreply.github.com>
2022-09-18 06:20:26 -0300

    Update webappEQUIPES.py

--- webappEQUIPES.py
+++ webappEQUIPES.py
@@ -276,7 +276,22 @@ elif choice == "EQUIPE 01":
     st.info('Dúvida(s) Enviada(s)')
     st.code(df01D['duvida']) 
     st.warning('Resposta do(a) TUTOR(A):')
-    st.code(df01R['resposta'])     
+    st.code(df01R['resposta'])   
+        
+elif choice == "EQUIPE 02":       
+    st.header("Painel Analítico: DÚVIDAS E RESPOSTAS")  
+    st.subheader('EQUIPE 02:')
+    colEQ21, colEQ22 = st.columns((1,1))
+    with colEQ21:
+        st.write("Nº TOTAL de Dúvidas:")
+        st.warning(NregDf02D)
+    with colEQ22:
+        st.write("Nº TOTAL de dúvidas RESPONDIDAS:")
+        st.info(NregDf02R)
+    st.info('Dúvida(s) Enviada(s)')
+    st.code(df02D['duvida']) 
+    st.warning('Resposta do(a) TUTOR(A):')
+    st.code(df02R['resposta'])    
         
 elif choice == "Texto_Markdown":
     st.subheader("Texto Markdown")
