72df2e138fa803ce7a47f036a3341322a20ed43a
mackweekCCT <112418297+mackweekCCT@users.noreply.github.com>
2022-10-29 18:48:20 -0300

    Update webappEQUIPES.py

--- webappEQUIPES.py
+++ webappEQUIPES.py
@@ -67,7 +67,6 @@ qr = qrcode.QRCode()
 qr.add_data('123456')
 qr.make(fit=True)
 image02 = qr.make_image(fill_color="darkblue", back_color="white")
-st.sidebar.image(image02, width=300, caption='QRcode') 
 
 #RESPOSTAS
 rR = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQw6XD9vI_C4zvZ6W51vut_Ze_D_OSESuXiHh1IAXeBFXRRvyQ7kyFTLbGip1obadjbZHUmaAxvXmnz/pub?gid=1789345467&single=true&output=csv')
@@ -124,6 +123,7 @@ NregDf11R = len(df11R)
 
 image01 = Image.open('ImagemLateral.jpg')
 st.sidebar.image(image01, width=300, caption='Mack Week CCT 2022') 
+st.image(image02, width=300, caption='QRcode') 
 st.title("PAINEL - EQUIPES HACKATHON")
 
 st.header("Principais Dúvidas:")
